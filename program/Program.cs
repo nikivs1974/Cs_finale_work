489e795ef307f7f880fc050b49dcc0d9b54c542b
Nikolay <nikivs2002@mail.ru>
2024-02-28 22:38:29 +0300

    create program

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Program
+string[] CreateNewArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+        count++;
+        }
+    }
+    string [] result = new string[count];
+        int temp = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+        result[temp] = array[i];
+        temp++;
+        }
+    }
+    return result;
+}
+
+// Print result
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+
+
+
+
+
+
+
+
+
+
